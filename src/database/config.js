8111d777fbdee2ae3d4bd17eeedd13dbc1617b81
Helen Euceda <neuceda13@gmail.com>
2026-02-13 03:15:52 -0600

    Feat: Creación de .gitignore y .env

--- src/database/config.js
+++ src/database/config.js
@@ -4,10 +4,11 @@ require('dotenv').config();
 const sequelize = new Sequelize(
   process.env.DB_NAME, 
   process.env.DB_USER, 
-  process.env.DB_PASS, 
+  process.env.DB_PASSWORD, 
   {
     host: process.env.DB_HOST,
     dialect: 'mysql',
+    port: process.env.DB_PORT || 3306,
     logging: false, 
   }
 );
